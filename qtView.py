1ebbefa030caa3a1e91482e3c1f36045295631ff
massimovassalli <massimo.vassalli@gmail.com>
2014-08-29 23:47:32 +0200

    Added the UI source file Added the pyqtgraph library source code

---
+++ qtView.py
@@ -0,0 +1,175 @@
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
+import os as os
+import sys
+import pyqtgraph as pg
+import numpy as np
+import Ui_qtView as qtView_face
+
+import experiment
+
+pg.setConfigOption('background', 'w')
+pg.setConfigOption('foreground', 'k')
+
+htmlpre = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/TR/REC-html40/strict.dtd">\n<html><head><meta name="qrichtext" content="1" /><style type="text/css">\np, li { white-space: pre-wrap; }\n</style></head><body style=" font-family:"Ubuntu"; font-size:11pt; font-weight:400; font-style:normal;">\n<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" font-size:8pt;">'
+htmlpost = '</span></p></body></html>'
+
+class curveWindow ( QtGui.QMainWindow ):
+    iter = 0
+    prev = 0
+    cRosso = QtGui.QColor(255,0,0)
+    cVerde = QtGui.QColor(50,255,50)
+    cNero = QtGui.QColor(0,0,0)
+    def __init__ ( self, parent = None ):
+        QtGui.QMainWindow.__init__( self, parent )
+        self.setWindowTitle( 'qtView' )
+        self.ui = qtView_face.Ui_facewindow()
+        self.ui.setupUi( self )
+        self.setConnections()
+
+        self.exp = experiment.experiment()
+
+    def addFiles(self, fnames = None):
+        if fnames == None:
+            fnames = QtGui.QFileDialog.getOpenFileNames(self, 'Select files', './')
+        app.processEvents()
+        pmax = len(fnames)
+
+        QtGui.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.WaitCursor))
+        progress = QtGui.QProgressDialog("Opening files...", "Cancel opening", 0, pmax);
+        i=0
+        for fname in fnames:
+            app.processEvents()
+            self.exp.addFiles([str(fname)])
+            progress.setValue(i)
+            i=i+1
+            if (progress.wasCanceled()):
+                break
+        progress.setValue(pmax)
+        QtGui.QApplication.restoreOverrideCursor()
+
+        self.refillList()
+
+    def addDirectory(self,dirname=None):
+        if dirname == None:
+            dirname = QtGui.QFileDialog.getExistingDirectory(self, 'Select a directory', './')
+            if not os.path.isdir(dirname):
+                return
+        app.processEvents()
+        pmax = len(os.listdir(dirname))
+
+        QtGui.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.WaitCursor))
+        progress = QtGui.QProgressDialog("Opening files...", "Cancel opening", 0, pmax);
+        i=0
+        for fnamealone in os.listdir(dirname):
+            #if i % 100 == 0:
+            app.processEvents()
+            fname = os.path.join(str(dirname), fnamealone)
+            self.exp.addFiles([str(fname)])
+            progress.setValue(i)
+            i=i+1
+            if (progress.wasCanceled()):
+                break
+        progress.setValue(pmax)
+        QtGui.QApplication.restoreOverrideCursor()
+        self.refillList()
+
+    def refillList(self):
+        scena = QtGui.QGraphicsScene()
+        width = self.ui.griglia.width()
+        height = self.ui.griglia.height()
+        N = len(self.exp)
+        self.ui.slide1.setMaximum(N)
+        self.ui.slide2.setMaximum(N)
+        gNx = np.sqrt(N*width/height)
+        Nx = int(np.ceil(gNx))
+        if int(gNx) == Nx:
+            Nx+=1
+        L = int(width/Nx)
+        i = 0
+        j = 0
+        if L<=3:
+            L=3
+        while i*Nx+j<N:
+            h = L-2
+            w = L-2
+            scena.addRect(QtCore.QRectF(j*(L)+1, i*(L)+1, h, w), pen = QtGui.QPen(self. cVerde,0) ,brush = self. cVerde )
+            j+=1
+            if j == Nx:
+                j=0
+                i+=1
+
+        scena.wheelEvent = self.scorri
+        self.ui.griglia.setScene(scena)
+#        og = self.ui.griglia.items()
+#        for i in range(len(og)):
+#            if self.curves[-i-1].inconsistency:
+#                og[i].setBrush(self.cRosso)
+#                og[i].setPen(self.cRosso)
+        self.ui.griglia.invalidateScene()
+
+        return True
+    def scorri(self,ev=None):
+        delta = ev.delta()/120
+        self.ui.slide2.setSliderPosition(self.ui.slide2.sliderPosition()-delta)
+    def sqSwitch(self,i,n):
+        og = self.ui.griglia.items()
+        if n:
+            c = self.cNero
+        else:
+            c = og[-i].brush().color()
+        og[-i].setPen(c)
+    def goToCurve(self,dove):
+        self.ui.labelNumber.setText('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/TR/REC-html40/strict.dtd">\n<html><head><meta name="qrichtext" content="1" /></head><body style=" font-family:\'Ubuntu\'; font-size:11pt; font-weight:400; font-style:normal;">\n<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" font-size:8pt;">{0}</span></p></body></html>'.format(dove))
+        self.ui.labFilename.setText(htmlpre + self.exp[dove-1].basename + htmlpost)
+        if self.prev != 0:
+            self.sqSwitch(self.prev,False)
+        self.sqSwitch(dove,True)
+        self.prev = dove
+        self.viewCurve(dove)
+
+    def viewCurve(self,dove = 1):
+        dove -= 1
+        self.ui.grafo.clear()
+        for p in self.exp[dove]:
+            if p == self.exp[dove][-1]:
+                self.ui.grafo.plot(p.z,p.f,pen='b')
+            else:
+                self.ui.grafo.plot(p.z,p.f)
+
+            self.ui.grafo.autoRange()
+
+    def setConnections(self):
+
+
+#        QtCore.QObject.connect(self.ui.slide1, QtCore.SIGNAL(_fromUtf8("actionTriggered(int)")), self.moveJumping)
+        QtCore.QObject.connect(self.ui.slide1, QtCore.SIGNAL(_fromUtf8("valueChanged(int)")), self.ui.slide2.setValue)
+        QtCore.QObject.connect(self.ui.slide2, QtCore.SIGNAL(_fromUtf8("valueChanged(int)")), self.ui.slide1.setValue)
+        QtCore.QObject.connect(self.ui.slide1, QtCore.SIGNAL(_fromUtf8("valueChanged(int)")), self.goToCurve )
+#        QtCore.QObject.connect(self.ui.slide2, QtCore.SIGNAL(_fromUtf8("actionTriggered(int)")), self.moveJumping)
+#        QtCore.QObject.connect(self.ui.slide2, QtCore.SIGNAL(_fromUtf8("sliderReleased()")), self.moveJumping)
+        #QtCore.QObject.connect(self.ui.slide1, QtCore.SIGNAL(_fromUtf8("valueChanged(int)")), self.goToCurve)
+
+        QtCore.QObject.connect(self.ui.bAddDir, QtCore.SIGNAL(_fromUtf8("clicked()")), self.addDirectory)
+        QtCore.QObject.connect(self.ui.bAddFiles, QtCore.SIGNAL(_fromUtf8("clicked()")), self.addFiles)
+        #QtCore.QObject.connect(self.ui.pushButton_3, QtCore.SIGNAL(_fromUtf8("clicked()")), self.switchColor)
+        #QtCore.QObject.connect(self.ui.pushButton, QtCore.SIGNAL(_fromUtf8("clicked()")), self.saveCurves)
+        #QtCore.QObject.connect(self.ui.pushButton_2, QtCore.SIGNAL(_fromUtf8("clicked()")), self.processNext)
+        #QtCore.QObject.connect(self.ui.spButton, QtCore.SIGNAL(_fromUtf8("clicked()")), facewindow.savePeaks)
+        #QtCore.QObject.connect(self.ui.pThreshold, QtCore.SIGNAL(_fromUtf8("editingFinished()")), self.refreshCurve)
+
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    app.setApplicationName( 'qtView' )
+    canale = curveWindow()
+    canale.show()
+    QtCore.QObject.connect( app, QtCore.SIGNAL( 'lastWindowClosed()' ), app, QtCore.SLOT( 'quit()' ) )
+    sys.exit(app.exec_())
